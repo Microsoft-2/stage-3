e0671a1d647536a402f41bd8b8e175367c894edf
Enrique <eph1806@gmail.com>
2025-12-10 14:46:07 +0000

    Crawler service

--- src/main/java/com/microsoft2/bigdata/search/application/CrawlerService.java
+++ src/main/java/com/microsoft2/bigdata/search/application/CrawlerService.java
@@ -1,5 +1,32 @@
 package com.microsoft2.bigdata.search.application;
 
+import java.util.UUID;
+
+import com.microsoft2.bigdata.search.domain.model.Document;
+import com.microsoft2.bigdata.search.domain.ports.DatalakeRepository;
+
 public class CrawlerService {
+    private final DatalakeRepository datalake;
+
+    // Inyectamos el puerto del datalake
+    public CrawlerService(DatalakeRepository datalake){
+        this.datalake = datalake;
+    }
+
+    // Ingestación de contenido
+    public String ingestContent(String content, String sourceUrl) {
+        // 1. Generar un ID único para el documento
+        String id = UUID.randomUUID().toString();
+
+        // 2. Adaptamos a la entidad de dominio
+        Document document = new Document(id, content);
+
+        // 3. Guardar al datalake
+        datalake.save(document);
+
+        System.out.println("Documento " + id + " descargado y guardado.");
+
+        return id;
+    }
 
 }
