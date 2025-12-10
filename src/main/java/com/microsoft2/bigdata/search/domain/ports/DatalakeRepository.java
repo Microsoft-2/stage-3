f8971f5ab308e55262e042b497d0385990379225
Enrique <eph1806@gmail.com>
2025-12-10 15:29:27 +0000

    Indexer y search engine dummy

--- src/main/java/com/microsoft2/bigdata/search/domain/ports/DatalakeRepository.java
+++ src/main/java/com/microsoft2/bigdata/search/domain/ports/DatalakeRepository.java
@@ -2,7 +2,7 @@ package com.microsoft2.bigdata.search.domain.ports;
 
 import com.microsoft2.bigdata.search.domain.model.Document;
 
-// El Crawler usará esto para guardar, el Indexer para leer.
+// El Crawler usará la interfaz para guardar, el Indexer para leer.
 public interface DatalakeRepository {
     void save(Document document);
     Document load(String documentId);
