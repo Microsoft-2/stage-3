272914cb862874205d650c3fbb25c863c52ab88f
Jorge Cubero Toribio <117381648+JorsuCT@users.noreply.github.com>
2025-12-16 19:46:46 +0000

    Update EventBus.java

--- src/main/java/com/microsoft2/bigdata/search/domain/ports/EventBus.java
+++ src/main/java/com/microsoft2/bigdata/search/domain/ports/EventBus.java
@@ -1,13 +1,10 @@
 package com.microsoft2.bigdata.search.domain.ports;
 
 public interface EventBus {
-    // Publicar un mensaje en un topic
     void publish(String topic, String message);
     
-    // Suscribirse a un topic y definir qué hacer cuando llegue un mensaje
     void subscribe(String topic, EventConsumer consumer);
 
-    // Interface funcional para el callback
     interface EventConsumer {
         void consume(String message);
     }
