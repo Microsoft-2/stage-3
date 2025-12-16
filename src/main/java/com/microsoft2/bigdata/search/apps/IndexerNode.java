288e2edff2335b4ec2a50f4e296e51d6b502f27d
Jorge Cubero Toribio <117381648+JorsuCT@users.noreply.github.com>
2025-12-16 19:44:32 +0000

    Update IndexerNode.java

--- src/main/java/com/microsoft2/bigdata/search/apps/IndexerNode.java
+++ src/main/java/com/microsoft2/bigdata/search/apps/IndexerNode.java
@@ -19,25 +19,20 @@ public class IndexerNode {
         String brokerUrl = System.getenv().getOrDefault("BROKER_URL", "tcp://localhost:61616");
         String datalakePath = System.getenv().getOrDefault("DATALAKE_PATH", "datalake_store");
 
-        // 1. Infraestructura
-        // IMPORTANTE: Hazelcast se unirá automáticamente al clúster si hay otros nodos
         Config config = HazelcastConfigFactory.createConfig();
         HazelcastInstance hz = Hazelcast.newHazelcastInstance(config); 
         IndexRepository indexRepo = new HazelcastIndexRepository(hz);
         DatalakeRepository datalake = new FileSystemDatalake(datalakePath);
         EventBus eventBus = new ActiveMQEventBus(brokerUrl);
 
-        // 2. Aplicación
         IndexerService indexer = new IndexerService(datalake, indexRepo);
 
-        // 3. Suscripción
         System.out.println("Indexer: Waiting for events on 'document.downloaded'...");
         eventBus.subscribe("document.downloaded", docId -> {
             System.out.println("Event received. Indexing: " + docId);
             indexer.indexDocument(docId);
         });
         
-        // Mantener vivo el programa (Hazelcast ya lo hace, pero por seguridad)
         synchronized (IndexerNode.class) {
             try { IndexerNode.class.wait(); } catch (InterruptedException e) {}
         }
